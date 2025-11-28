1db142725adc14a162538e33124ee56deb979ae1
Nathanaelopes <lopesnathanael78@gmail.com>
2025-11-28 18:03:42 -0300

    Add files via upload

---
+++ nathan.js
@@ -0,0 +1,151 @@
+const dadosJogos = {
+    'Gta 5': {
+        texto: 'The Last Of Us é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia.',
+        img: 'https://s2-techtudo.glbimg.com/Dx-d7zd6abaVIdY1QqGVKl_QvJ4=/0x0:620x349/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/s/i/R6L1HDR3GxheHSo2ECLQ/2013-10-11-grand-theft-auto-5-gta-finais.jpg'
+    },
+    'The Last Of Us': {
+        texto: 'The Last Of Us é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia.',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRX5aWovXY0igXX4MizTcqBdYyYhsSilaifLg&s'
+    },
+    'Call of Duty: Black Ops': {
+        texto: 'Call of Duty: Black Ops é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. The Last Of Us é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. ',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw9yzmqnTFYVW9_ivEV7495ebr-6KgrxvRvw&s'
+    },
+    'Super Mario Bros': {
+        texto: 'Super Mario Bros é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. The Last Of Us é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. ',
+        img: 'https://upload.wikimedia.org/wikipedia/pt/2/2e/New_Super_Mario_Bros._-_Norh-american_cover.jpg'
+    },
+    'Fortinite': {
+        texto: 'Fortinite é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. The Last Of Us é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. ',
+        img: 'https://i.redd.it/tkq453ttioq51.jpg'
+    },
+    'Minecraft': {
+        texto: 'Minecraft é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. The Last Of Us é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia.',
+        img: 'https://preview.redd.it/2zs6alhs84y71.png?width=767&format=png&auto=webp&s=15fae2f6e601178e7aae481c70dff1d229bb8855'
+    },
+
+    'God Of War': {
+        texto: 'The Last Of Us é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia..',
+        img: 'https://meups.com.br/wp-content/uploads/2021/04/CAPA-God-of-WAR.jpg'
+    },
+    'Bloodborne': {
+        texto: 'The Last Of Us é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. The Last Of Us é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. ',
+        img: 'https://blackcompany.com.br/wp-content/uploads/2025/02/Capa-oficial-de-Bloodborne-1024x576.png'
+    },
+    'Os Incriveis': {
+        texto: 'Call of Duty: Black Ops é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. The Last Of Us é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia.  ',
+        img: 'https://image.api.playstation.com/cdn/UP1018/CUSA09878_00/XvIVyw8RZPgz3YgfPKw9eG6PqJZZyh3N.png'
+    },
+    'Dragon Ball Z': {
+        texto: 'Super Mario Bros é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. The Last Of Us é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia.   ',
+        img: 'https://http2.mlstatic.com/D_NQ_NP_802813-MLB26642521545_012018-O.webp'
+    },
+    'Mickey': {
+        texto: 'Fortinite é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. The Last Of Us é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. ',
+        img: 'https://image.api.playstation.com/vulcan/ap/rnd/202402/0507/94c74df621a8b0f4d957fdd09492318d15e41b577297b795.jpg'
+    },
+    'Demon souls': {
+        texto: 'Minecraft é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. The Last Of Us é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. Grand Theft Auto V é um jogo eletrônico de ação-aventura que se passa no estado ficcional de San Andreas, baseado no sul da Califórnia. ',
+        img: 'https://image.api.playstation.com/vulcan/img/rnd/202011/1717/GemRaOZaCMhGxQ9dRhnQQyT5.png'
+    },
+}
+
+function sinopse(titulo){
+    const jogo = dadosJogos[titulo];
+    
+    // Verifica se o jogo existe no banco de dados
+    if (!jogo) {
+        console.error(`Jogo "${titulo}" não encontrado.`);
+        return;
+    }
+
+    document.getElementById("titulo").innerHTML = titulo;
+    document.getElementById("texto").innerHTML = jogo.texto;
+    document.getElementById("img").src = jogo.img;
+
+    document.getElementById("sinopse").style.display="block";
+    document.getElementById("overlay").style.display="block";
+}
+
+function Openprodutos2() {
+    document.querySelector(".produtos").style.display = "none";
+    document.querySelector(".produtos2").style.display = "flex";   
+}
+
+function voltar() {
+    
+    document.querySelector(".produtos").style.display = "flex";
+    document.querySelector(".produtos2").style.display = "none";
+     
+}
+
+function fechar(){
+    document.getElementById("sinopse").style.display="none";
+    document.getElementById("overlay").style.display="none";
+}
+
+
+
+ let index = 0;
+const slides = document.querySelectorAll("#carrossel .slide");
+
+function trocarSlide() {
+    slides[index].classList.remove("ativo"); 
+
+    index = (index + 1) % slides.length; 
+
+    slides[index].classList.add("ativo");
+}
+
+setInterval(trocarSlide, 6000);  
+
+// Defina a data final da promoção
+let countdownDate = new Date("2026-04-14 23:59:59").getTime();
+
+let timer = setInterval(function () {
+    let now = new Date().getTime();
+    let distance = countdownDate - now;
+
+    // Cálculo
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Mostrar na tela
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    // Quando acabar
+    if (distance < 0) {
+        clearInterval(timer);
+        document.getElementById("time").innerHTML = "PROMOÇÃO ENCERRADA";
+    }
+}, 1000);
+
+/*
+function atualizarCarrinho() {
+    const carrinhoConteudo = document.getElementById('carrinho-conteudo');
+    carrinhoConteudo.innerHTML = '';
+    
+    if (carrinho.length === 0) {
+        carrinhoConteudo.innerHTML = '<p>Seu carrinho está vazio.</p>';
+    } else {
+        carrinho.forEach(item => {
+            carrinhoConteudo.innerHTML += `<p>${item.nome} - R$ ${item.preco.toFixed(2)}</p>`;
+        });
+    }
+}
+
+function finalizarCompra() {
+    if (carrinho.length > 0) {
+        alert('Compra finalizada com sucesso!');
+        carrinho = [];
+        atualizarCarrinho();
+    } else {
+        alert('Seu carrinho está vazio.');
+    }
+}
+    */
